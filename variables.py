33c76c10eab9fb7c433e521ad70035b02e59b160
AMBARISH BELVAI <ambarishbelvai12@gmail.com>
2026-03-09 22:18:57 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,29 @@
+#variables and data type
+#string (text)
+name = "AMBARISH"
+print("My name is",name)
+
+#integer (whole number)
+age =  20
+print("My age is",age)
+
+#float (decimal number)
+height = 5.9
+print("My height is",height)
+
+# boolean (True or false)
+is_student = True
+print("Are you student",is_student)
+
+#multiple variables
+x = 10
+y = 20
+z = x+y
+print("Sum:",z)
+
+#changing variable
+count = 5
+print("count is",count)
+
+count = count+1
+print("count is now:",count)
